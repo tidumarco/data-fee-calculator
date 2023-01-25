a9c4b8d020bdb39a98e0decbf3e453b7f36d40b4
Marco Tidu <tidumarco@gmail.com>
2023-01-25 08:44:59 +0200

    Assignment succesfully satisfying rules

--- src/types.tsx
+++ src/types.tsx
@@ -1,8 +1,23 @@
 type DeliveryFee = number;
 type Distance = number;
 type Items = number;
-type DeliveryDate = string;
+type DeliveryDate = Date;
 type onChange = React.ChangeEvent<HTMLInputElement>;
 type CartValue = number;
+type Rush = boolean;
+type CartSurcharge = number;
+type DistanceSurcharge = number;
+type ItemsSurcharge = number;
 
-export type { CartValue, DeliveryFee, Distance, Items, DeliveryDate, onChange };
+export type {
+  CartValue,
+  DeliveryFee,
+  Distance,
+  Items,
+  DeliveryDate,
+  onChange,
+  Rush,
+  CartSurcharge,
+  DistanceSurcharge,
+  ItemsSurcharge,
+};
